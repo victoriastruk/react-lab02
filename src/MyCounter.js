c55a3ea1019d2babbf6f95c7d72e35b4c1929712
Victoria Struk <struk.victoria@gmail.com>
2022-12-06 03:35:14 +0200

    list counters

--- src/MyCounter.js
+++ src/MyCounter.js
@@ -1,37 +1,66 @@
 import { React, useState } from 'react'
 import './App.css'
 
-export default function MyCounter( props ) {
-  const [min] = useState( -10 );
-  const [max] = useState( 10 );
-  const [counter, setCounter] = useState( 0 );
+const counters = [
+  { id: 1, initial: 6, min: -5, max: 10 },
+  { id: 2, initial: 5, },
+  { id: 3, },
+];
 
-  //increase counter
-  const increase = () => {
-    if ( counter < max )
-      setCounter( count => count + 1 );
+
+
+export default function MyCounter() {
+  for(let i = 0; i<counters.length; i++){
+    if(counters[i].hasOwnProperty('initial') === false){
+        counters[i].initial = 0;
+    } else if(counters[i].hasOwnProperty('min') === false){
+      counters[i].min = -10;
+    } else if(counters[i].hasOwnProperty('max') === false){
+      counters[i].max = 10;
+    }
+  }
+  
+
+const [counts, setCounters] = useState( counters );
+
+  const increase = ( id ) => {
+    const updatedCounters = counts.map( el =>
+      ( el.id === id && el.initial < el.max )
+        ? { ...el, initial: el.initial + 1 }
+        : el);
+    setCounters( updatedCounters );
   };
 
   //decrease counter
-  const decrease = () => {
-    if ( counter > min )
-      setCounter( count => count - 1 );
+  const decrease = ( id ) => {
+    const updatedCounters = counts.map( el => 
+      (el.id === id && el.initial > el.min)
+      ? { ...el, initial: el.initial - 1 }
+      : el );
+    setCounters( updatedCounters );
   };
 
   //reset counter
-  const reset = () => {
-    setCounter( 0 );
+  const reset = ( id ) => {
+    const updatedCounters = counts.map
+    ( el => el.id === id
+      ? { ...el, initial: 0 }
+      : el );
+    setCounters( updatedCounters );
   }
 
   return (
-    <div className="counter">
-      Поточний рахунок:
-      <span className="counter__output">{counter}</span>
-      <div className="btn__container">
-        <button className="control__btn" onClick={increase}>+</button>
-        <button className="control__btn" onClick={decrease}>-</button>
-        <button className="reset" onClick={reset}>Reset</button>
-      </div>
-    </div>
+    <ul>
+      {counts.map( el => (
+        <li key={el.id}>
+          <div className="counter">
+            Поточний рахунок:
+            <span className="counter__output">{el.initial}</span>
+            <div className="btn__container">
+              <button className="control__btn" onClick={() => increase( el.id )}>+</button>
+              <button className="control__btn" onClick={() => decrease( el.id )}>-</button>
+              <button className="reset" onClick={() => reset( el.id )}>Reset</button>
+            </div></div></li> ))}
+    </ul>
   )
-}
+} 
